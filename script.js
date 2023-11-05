9e0d19a5665ffb24cc79d4fcfc536f50833f7555
HomayraOitizzo <hummuoitizzo@gmail.com>
2023-11-06 01:03:11 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let addTocartbuttons = document.querySelectorAll('.add');
+    let cartItems = document.querySelector('.cart-items');
+
+
+    addTocartbuttons.forEach(button =>{
+        button.addEventListener('click', function(){
+            let product = this.parentElement;
+            let productName = product.querySelector('h4').textContent;
+             let productPrice = product.querySelector('p').textContent;
+
+             let cartItem = document.createElement('li');
+             cartItem.textContent = `${productName} - ${productPrice}`;
+
+
+
+             cartItems.appendChild(cartItem);
+            
+
+          
+
+
+           
+
+
+
+        });
+        const removeButtons = document.querySelectorAll(".remove-button");
+
+        removeButtons.forEach(function(button) {
+            button.addEventListener("click", function() {
+                
+                const productId = this.getAttribute("data-product-id");
+
+                
+                const cartItems = document.querySelectorAll('.cart-items li');
+                cartItems.forEach(function(cartItem) {
+                    if (cartItem.getAttribute("data-product-id") === productId) {
+                        cartItem.remove();
+                    }
+                });
+            });
+        });
+    });
+});
+
+
+
+
+
+
+
+    
+    
+
+
+ 
+
+
+
+
+
+
+
+
